9dfb22830a1423368b3200c2dead00f03ef2473f
hanlee <me@hanlee.co>
2024-09-02 17:14:46 -0700

    chore: add nuxt modules and setup eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,34 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  // https://nuxt.com/docs/api/nuxt-config#compatibilitydate
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+
+  // https://nuxt.com/docs/api/nuxt-config#future
+  future: {
+    // https://nuxt.com/docs/api/nuxt-config#compatibilityversion
+    compatibilityVersion: 4,
+  },
+
+  // https://nuxt.com/docs/api/nuxt-config#modules-1
+  modules: [
+    '@vueuse/nuxt',
+    '@unocss/nuxt',
+    '@nuxt/eslint',
+    '@nuxthub/core',
+  ],
+
+  // https://devtools.nuxt.com/guide/getting-started
+  devtools: { enabled: true },
+
+  // https://nuxt.com/docs/api/nuxt-config#css
+  css: [
+    '@unocss/reset/tailwind.css',
+  ],
+
+  // https://eslint.nuxt.com/packages/module
+  eslint: {
+    config: {
+      standalone: false,
+    },
+  }
 })
