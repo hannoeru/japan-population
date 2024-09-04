45ff9089d0ad9380d900835956719b0b59e53c60
hanlee <me@hanlee.co>
2024-09-03 22:18:56 -0700

    feat: remove prefecture select title and add clear selected button

--- app/components/PrefectureSelects.vue
+++ app/components/PrefectureSelects.vue
@@ -13,11 +13,6 @@ const selected = defineModel('selected', {
 
 <template>
   <section>
-    <header class="p-6">
-      <h2 class="h2">
-        都道府県を選択：
-      </h2>
-    </header>
     <div class="grid grid-cols-3 gap-1 px-2 md:grid-cols-5 sm:px-4 sm:pl-8">
       <template v-for="prefecture of props.prefectures" :key="prefecture.prefCode">
         <Checkbox
@@ -29,6 +24,11 @@ const selected = defineModel('selected', {
           :disabled="false"
         />
       </template>
+    </div>
+    <div class="flex justify-center p-4">
+      <button class="btn" title="選択項目をクリア" :disabled="!selected.length" @click="selected = []">
+        選択項目をクリア
+      </button>
     </div>
   </section>
 </template>
