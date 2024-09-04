8779fd255e0c20922aa6208f94d14dcf470e7cba
hanlee <me@hanlee.co>
2024-09-03 22:13:06 -0700

    chore: add command

--- server/api/prefectures.ts
+++ server/api/prefectures.ts
@@ -3,6 +3,10 @@ import type { PrefecturesResponse } from '~~/types/resas'
 
 export default defineCachedEventHandler(async (event) => {
   const resasClient = useResasClient(event)
+  /**
+   * 都道府県一覧を取得する
+   * @see https://opendata.resas-portal.go.jp/docs/api/v1/prefectures.html
+   */
   const { result } = await resasClient<PrefecturesResponse>('/api/v1/prefectures')
   return result
 }, {
