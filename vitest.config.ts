ad207399cfd717045a9f8e4a5aa7b3995432a1ae
hanlee <me@hanlee.co>
2024-09-03 20:44:16 -0700

    chore: ignore vue experimental warning message on vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,16 @@
 import { defineVitestConfig } from '@nuxt/test-utils/config'
 
+const ignoreMessages = [
+  '<Suspense> is an experimental feature and its API will likely change.',
+]
+
 export default defineVitestConfig({
   test: {
     environment: 'nuxt',
+    onConsoleLog(log) {
+      if (ignoreMessages.some(message => log.includes(message))) {
+        return false
+      }
+    },
   },
 })
